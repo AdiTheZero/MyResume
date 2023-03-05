7a4e37d0fb1d88300b66282174de923e255e2afe
AdiTheZero <98465446+AdiTheZero@users.noreply.github.com>
2023-03-05 23:18:22 +0530

    Update App.js
    change

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-
+ 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
          
